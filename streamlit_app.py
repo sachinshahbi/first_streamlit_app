ed487f18351ce1ca74170e903729e4a61ac20229
Sachin <99244236+sachinshahbi@users.noreply.github.com>
2023-05-03 11:59:33 +0530

    Update streamlit_app.py
    Added content in the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents first healthy dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Jowari Upma')
+streamlit.text('Ragi Dosa')
+streamlit.text('Thepla')
