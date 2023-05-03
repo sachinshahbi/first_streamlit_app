8244830d4eaf8ec35f6f3e18915cb4a55371e9cf
Sachin <99244236+sachinshahbi@users.noreply.github.com>
2023-05-03 12:01:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My parents first healthy dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Jowari Upma')
-streamlit.text('Ragi Dosa')
-streamlit.text('Thepla')
+streamlit.text('🥣Jowari Upma')
+streamlit.text('🥗Ragi Dosa')
+streamlit.text('🍞Thepla')
